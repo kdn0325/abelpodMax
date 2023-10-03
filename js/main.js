3b5c0352deb288b08c2e1f55c4c53353fa265471
Abel <dn10003@gmail.com>
2023-10-04 03:10:02 +0900

    #1_애플 인터렉션 웹

--- js/main.js
+++ js/main.js
@@ -124,14 +124,14 @@
     let imgElem;
     for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
       imgElem = document.createElement("img"); // === new Image();
-      imgElem.src = `./video/001/IMG_${1 + i}.JPG`;
+      imgElem.src = `./video/001/IMG_${1 + i}.jpg`;
       sceneInfo[0].objs.videoImages.push(imgElem);
     }
 
     let imgElem2;
     for (let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
       imgElem2 = document.createElement("img"); // === new Image();
-      imgElem2.src = `./video/002/IMG_${1 + i}.JPG`;
+      imgElem2.src = `./video/002/IMG_${1 + i}.jpg`;
       sceneInfo[2].objs.videoImages.push(imgElem2);
     }
 
